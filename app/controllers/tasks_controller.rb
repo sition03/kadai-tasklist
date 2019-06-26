6ecd86d453cd35ba1413330d29d8427a3a51f056
takuto.futakawame <takuto3213@gmail.com>
2019-06-26 13:39:59 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,61 +1,57 @@
 class TasksController < ApplicationController
-  before_action :set_task,only:[:show,:edit,:update,:destroy]   
+  before_action :set_task,only: [:show,:edit,:update,:destroy]   
     
 def index 
-    @tasks=Task.all
+  @tasks = Task.all
 end
 
 def new
-  @task=Task.new
+  @task = Task.new
 end
 
 def show 
-  set_task
 end
 
 def edit 
-  set_task
 end
 
 def create
-    @task=Task.new(task_params)
+  @task = Task.new(task_params)
     
-    if @task.save
-        flash[:dark]="タスクが投稿されました"
-        redirect_to @task
-    else
-        flash.now[:danger]="タスクは投稿されませんでした"
-        render:new
-    end
+  if @task.save
+    flash[:dark]="タスクが投稿されました"
+    redirect_to @task
+  else
+    flash.now[:danger]="タスクは投稿されませんでした"
+    render :new
+  end
 end
 
 def update
-    set_task
     
-    if @task.update(task_params)
-        flash[:dark]="タスクは正常に更新されました"
-        redirect_to @task
-    else
-        flash.now[:danger]="タスクは更新されませんでした"
-        render:edit
-    end
+  if @task.update(task_params)
+    flash[:dark]="タスクは正常に更新されました"
+    redirect_to @task
+  else
+    flash.now[:danger]="タスクは更新されませんでした"
+    render :edit
+  end
 end
 
 def destroy
-    set_task
-    @task.destroy
-    flash[:success]="タスクは正常に削除されました"
-    redirect_to tasks_url
+  @task.destroy
+  flash[:success]="タスクは正常に削除されました"
+  redirect_to tasks_url
 end
 
 private 
 
 def set_task
-    @task=Task.find(params[:id])
+  @task = Task.find(params[:id])
 end
 
 def task_params
-    params.require(:task).permit(:content,:status)
+  params.require(:task).permit(:content,:status)
 end
 end
 
