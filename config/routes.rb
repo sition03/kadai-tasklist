7fed37a0e88e4b657763216ac2cb78dd9731de6d
takuto.futakawame <takuto3213@gmail.com>
2019-07-09 05:58:02 +0000

    error4

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   get "signup", to: "users#new"
   resources :tasks
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :index]
 end
